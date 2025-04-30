4f4d6c2c16a57c5bdf529176f9823d9df617bbc9
JayS0223 <jay.s@videosdk.live>
2025-04-30 17:51:17 +0530

    added the flutter rive  example

---
+++ flutter-rive-example/README.md
@@ -0,0 +1,1 @@
+# Character - Rive- Example
---
+++ flutter-rive-example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <rive_common/rive_plugin.h>
+#include <videosdk_webrtc/flutter_web_r_t_c_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) rive_common_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "RivePlugin");
+  rive_plugin_register_with_registrar(rive_common_registrar);
+  g_autoptr(FlPluginRegistrar) videosdk_webrtc_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterWebRTCPlugin");
+  flutter_web_r_t_c_plugin_register_with_registrar(videosdk_webrtc_registrar);
+}
